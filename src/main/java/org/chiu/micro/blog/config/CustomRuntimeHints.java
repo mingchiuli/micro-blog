95071b13c6de4c2fa8f451e3fddab96fdb5b9bb3
limingjiu <limingjiu@kanzhun.com>
2024-06-27 11:27:08 +0800

    fix

--- src/main/java/org/chiu/micro/blog/config/CustomRuntimeHints.java
+++ src/main/java/org/chiu/micro/blog/config/CustomRuntimeHints.java
@@ -19,7 +19,9 @@ public class CustomRuntimeHints implements RuntimeHintsRegistrar {
         hints.reflection().registerConstructor(ListValueConstraintValidator.class.getDeclaredConstructor(), ExecutableMode.INVOKE);
 
         hints.serialization().registerType(BlogOperateMessage.class);
-        hints.serialization().registerType(Number.class);
+        hints.serialization().registerType(Integer.class);
+        hints.serialization().registerType(Long.class);
+
         // Register resources
         hints.resources().registerPattern("ValidationMessages.properties");
         hints.resources().registerPattern("script/blog-delete.lua");

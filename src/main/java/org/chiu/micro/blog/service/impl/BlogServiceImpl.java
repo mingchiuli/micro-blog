1731140f28ac00c03ac9cdf38dd451d4cb305ff0
mingchiuli <mingchiuli@163.com>
2024-06-28 23:47:11 +0800

    rillback

--- src/main/java/org/chiu/micro/blog/service/impl/BlogServiceImpl.java
+++ src/main/java/org/chiu/micro/blog/service/impl/BlogServiceImpl.java
@@ -219,7 +219,7 @@ public class BlogServiceImpl implements BlogService {
         }
 
         BeanUtils.copyProperties(blog, blogEntity);
-        BlogEntity saved = blogRepository.saveAndFlush(blogEntity);
+        BlogEntity saved = blogRepository.save(blogEntity);
 
         // 通知消息给mq,更新并删除缓存
         // 防止重复消费

4ae832d6f6b1cbe50bf204e88042dd7aff3eb368
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-07-05 14:52:35 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ tasks.named<BootBuildImage>("bootBuildImage") {
 		"BP_NATIVE_IMAGE_BUILD_ARGUMENTS" to
 			"""
 				-march=compatibility
-				--gc=epsilon
+				--gc=serial
 				-R:MaxHeapSize=256m
 				-O2
 			""",

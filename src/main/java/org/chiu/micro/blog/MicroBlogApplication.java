d614aa70945059e5e5d29aec06d8c5a92042a19a
mingchiuli <80095372+mingchiuli@users.noreply.github.com>
2024-07-25 10:02:13 +0800

    Update MicroBlogApplication.java

--- src/main/java/org/chiu/micro/blog/MicroBlogApplication.java
+++ src/main/java/org/chiu/micro/blog/MicroBlogApplication.java
@@ -18,7 +18,6 @@ import java.util.TimeZone;
 public class MicroBlogApplication {
 
 	public static void main(String[] args) {
-			System.out.println(TimeZone.getDefault());
 			TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
 			SpringApplication.run(MicroBlogApplication.class, args);
 	}

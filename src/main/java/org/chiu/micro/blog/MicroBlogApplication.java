5cd61c64648ce33981333bd2df0420043cd2a351
mingchiuli <mingchiuli@163.com>
2024-07-25 09:06:48 +0800

    test

--- src/main/java/org/chiu/micro/blog/MicroBlogApplication.java
+++ src/main/java/org/chiu/micro/blog/MicroBlogApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
 
 @SpringBootApplication(proxyBeanMethods = false)
 @EnableAsync
@@ -17,6 +18,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MicroBlogApplication {
 
 	public static void main(String[] args) {
+			System.out.println(TimeZone.getDefault());
+			TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
 			SpringApplication.run(MicroBlogApplication.class, args);
 	}
 

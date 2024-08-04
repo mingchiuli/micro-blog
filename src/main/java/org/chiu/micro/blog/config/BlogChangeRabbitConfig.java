1cc45419159e94bfede4f32cc821f9b74d65b8f1
mingchiuli <mingchiuli@163.com>
2024-08-04 17:15:55 +0800

    update

---
+++ src/main/java/org/chiu/micro/blog/config/BlogChangeRabbitConfig.java
@@ -0,0 +1,62 @@
+package org.chiu.micro.blog.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author mingchiuli
+ * @create 2022-12-25 4:13 pm
+ */
+@Configuration
+public class BlogChangeRabbitConfig {
+
+    public static final String ES_QUEUE = "blog.change.queue.es";
+
+    public static final String CACHE_QUEUE = "blog.change.queue.cache";
+
+    public static final String TOPIC_EXCHANGE = "blog.change.topic.exchange";
+
+    public static final String ES_BINDING_KEY = "blog.change.binding.es";
+
+    public static final String CACHE_BINDING_KEY = "blog.change.binding.cache";
+
+    @Bean("esQueue")
+    Queue esQueue() {
+        return new Queue(ES_QUEUE, true, false, false);
+    }
+
+    @Bean("cacheQueue")
+    Queue cahceQueue() {
+        return new Queue(CACHE_QUEUE, true, false, false);
+    }
+
+    //ES交换机
+    @Bean("topicExchange")
+    TopicExchange exchange() {
+        return new TopicExchange(TOPIC_EXCHANGE, true, false);
+    }
+
+    //绑定ES队列和ES交换机
+    @Bean("esBinding")
+    Binding esBinding(@Qualifier("esQueue") Queue esQueue,
+                    @Qualifier("topicExchange") TopicExchange esExchange) {
+        return BindingBuilder
+                .bind(esQueue)
+                .to(esExchange)
+                .with(ES_BINDING_KEY);
+    }
+
+    @Bean("cacheBinding")
+    Binding cacheBinding(@Qualifier("cacheQueue") Queue cacheQueue,
+                    @Qualifier("topicExchange") TopicExchange esExchange) {
+        return BindingBuilder
+                .bind(cacheQueue)
+                .to(esExchange)
+                .with(ES_BINDING_KEY);
+    }
+}
--- src/main/java/org/chiu/micro/blog/listener/BlogOperateEventListener.java
+++ src/main/java/org/chiu/micro/blog/listener/BlogOperateEventListener.java
@@ -2,6 +2,7 @@ package org.chiu.micro.blog.listener;
 
 import lombok.RequiredArgsConstructor;
 import org.chiu.micro.blog.event.BlogOperateEvent;
+import org.chiu.micro.blog.config.BlogChangeRabbitConfig;
 import org.chiu.micro.blog.constant.BlogOperateMessage;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.event.EventListener;
@@ -15,25 +16,15 @@ public class BlogOperateEventListener {
 
     private final RabbitTemplate rabbitTemplate;
 
-    private static final String ES_EXCHANGE = "es.direct.exchange";
-
-    private static final String ES_BINDING_KEY = "es.binding";
-
-    private static final String CACHE_BLOG_EVICT_EXCHANGE = "cache.blog.direct.exchange";
-
-    private static final String CACHE_BLOG_EVICT_BINDING_KEY = "cache.blog.evict.binding";
+    private static final String BINDING_KEY_MODE = "blog.change.binding.#";
 
     @EventListener
     @Async("commonExecutor")
     public void process(BlogOperateEvent event) {
         BlogOperateMessage messageBody = event.getBlogOperateMessage();
 
-        rabbitTemplate.convertAndSend(ES_EXCHANGE,
-                ES_BINDING_KEY,
-                messageBody);
-
-        rabbitTemplate.convertAndSend(CACHE_BLOG_EVICT_EXCHANGE,
-                CACHE_BLOG_EVICT_BINDING_KEY,
+        rabbitTemplate.convertAndSend(BlogChangeRabbitConfig.TOPIC_EXCHANGE,
+                BINDING_KEY_MODE,
                 messageBody);
     }
 }
